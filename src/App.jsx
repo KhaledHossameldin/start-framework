582dd24fe12f0491158f8bd2ba0107ca29340942
Khaled Hossameldin <33874711+KhaledHossameldin@users.noreply.github.com>
2023-09-10 22:29:23 +0300

    added navbar + renamed Home to Layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 import 'bootstrap/dist/js/bootstrap.bundle';
-import Home from './components/home/Home';
+import Layout from './components/layout/Layout';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import About from './components/about/About';
 import Portfolio from './components/portfolio/Portfolio';
 import Contact from './components/contact/Contact';
+import Home from './components/home/Home';
 
 const router = createBrowserRouter([
   {
-    path: '/', element: <Home />, children: [
-      { path: '/', element: <About /> },
+    path: '/', element: <Layout />, children: [
+      { path: '/', element: <Home /> },
       { path: 'about', element: <About /> },
       { path: 'portfolio', element: <Portfolio /> },
       { path: 'contact', element: <Contact /> },
-      
+
       {
         path: '*', element: <div className='d-flex justify-content-center align-items-center vh-100'>
           <h1>Not Found</h1>
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+
+import './navbar.css';
+
+let navbarLinks;
+
+export default class Navbar extends Component {
+
+    clearActiveLinks = _ => {
+        for (let i = 0; i < navbarLinks.length; i++) {
+            navbarLinks[i].parentElement.classList.remove('active');
+        }
+    }
+
+    chooseTab = e => {
+        for (let i = 0; i < navbarLinks.length; i++) {
+            if (e.target == navbarLinks[i]) {
+                navbarLinks[i].parentElement.classList.add('active');
+            } else {
+                navbarLinks[i].parentElement.classList.remove('active');
+            }
+        }
+    }
+
+    componentDidMount = _ => navbarLinks = document.querySelectorAll('nav li.nav-item a.nav-link');
+
+    render() {
+        return (
+            <nav className="navbar navbar-dark navbar-expand-lg py-4">
+                <div className="container">
+                    <Link onClick={this.clearActiveLinks} className="navbar-brand text-uppercase fs-2 fw-bolder" to="">Start Framework</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li className="nav-item me-3">
+                                <Link onClick={this.chooseTab} className="nav-link text-uppercase text-white fw-bold" aria-current="page" to="about">About</Link>
+                            </li>
+                            <li className="nav-item me-3">
+                                <Link onClick={this.chooseTab} className="nav-link text-uppercase text-white fw-bold" to="portfolio">Portfolio</Link>
+                            </li>
+                            <li className="nav-item me-3">
+                                <Link onClick={this.chooseTab} className="nav-link text-uppercase text-white fw-bold" to="contact">Contact</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        )
+    }
+}
