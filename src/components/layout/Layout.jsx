b65173e1bb004b37b21b1193f56402146faf4fb9
Khaled Hossameldin <33874711+KhaledHossameldin@users.noreply.github.com>
2023-09-11 11:54:18 +0300

    fixed bugs

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -3,16 +3,12 @@ import Navbar from '../navbar/Navbar';
 import { Outlet } from 'react-router-dom';
 import Footer from '../footer/Footer';
 
-import './layout.css';
-
 export default class Layout extends Component {
     render() {
         return (
             <>
                 <Navbar />
-                <div id='content'>
-                    <Outlet />
-                </div>
+                <Outlet />
                 <Footer />
             </>
         )
